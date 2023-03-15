b2b8b7363bbb2b2b66202dd1b91fcf30af5449c5
nikita9954 <kunitsa.nikita2@gmail.com>
2023-03-15 17:53:36 +0300

    done

--- src/main/java/src/Json/Main.java
+++ src/main/java/src/Json/Main.java
@@ -1,10 +1,9 @@
 package src.Json;
 
-
-
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import java.io.IOException;
 import java.net.URL;
-
 public class Main {
     public static void main(String[] args) throws IOException {
         ObjectMapper objectMapper=new ObjectMapper();
